cf5ffd3a102c8ea24145df94444314f5bfdac16e
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-01 23:25:41 +0600

    display project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,36 +3,38 @@ import React from 'react';
 const ProjectCard = ({ project }) => {
     return (
         <div>
-            <a href="#" className="hover-3d my-12 mx-2 cursor-pointer">
+            <a className="hover-3d my-12 mx-2 cursor-pointer">
 
                 {/* content */}
-                <div className="card">
+                <div className="card border border-[#ABB2BF] rounded-none">
                     <div>
-                        <img src={project.thumbnail} alt={project.title} />
+                        <img src={project.thumbnail} alt={project.title} className="w-full h-50 object-cover" />
                     </div>
-                    <div>
+                    <div className='border-b border-[#ABB2BF] p-2 '>
                         <h2>{project.tag}</h2>
                     </div>
-                    <div>
-                        <h2>{project.title}</h2>
-                        <p>{project.subtitle}</p>
-                    </div>
-                    <div>
-                        {project.actions.map((action, index) => (
-                            <a key={index} href={action.href} className="text-[#C778DD]">{action.label}</a>
-                        ))}
+                    <div className='p-4 space-y-4'>
+                        <div>
+                            <h2 className='font-2xl font-medium'>{project.title}</h2>
+                            <p className='font-[16px] text-[#ABB2BF]'>{project.subtitle}</p>
+                        </div>
+                        <div className='flex gap-4 flex-wrap'>
+                            {project.actions.map((action, index) => (
+                                <a key={index} href={action.href} className="text-[#C778DD] btn bg-transparent border border-[#ABB2BF] px-4 py-2 rounded-none" target='_blank'>{action.label}</a>
+                            ))}
+                        </div>
                     </div>
                 </div>
 
                 {/* 8 empty divs needed for the 3D effect */}
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
+                <div className="pointer-events-none"></div>
             </a>
         </div>
     );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,16 +4,19 @@ import ProjectCard from './ProjectCard';
 const Projects = ({ projects }) => {
 
     const projectData = use(projects);
-    return (
-        <div>
-            <h1>#projects</h1>
-            <hr />
 
-            <div>
+    return (
+        <div className='w-4/5 mx-auto bg-[#282C33]'>
+            <div className='flex gap-5 items-center'>
+                <h1 className='text-3xl font-medium'><span className='text-[#C778DD]'>#</span>projects</h1>
+                <div className='border border-[#C778DD] h-0 w-full'></div>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
                 {
                     projectData.map(project => <ProjectCard key={project.title} project={project} />)
                 }
-                
+
             </div>
         </div>
     );
