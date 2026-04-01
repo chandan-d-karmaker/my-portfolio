7cfaa65f527d35c0754b13b087c059c64be44f16
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-01 17:56:54 +0600

    added links to sidebar socials

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,9 +9,15 @@ const Sidebar = () => {
             <div className='md:flex hidden flex-col gap-5 fixed right-370 top-0 justify-center items-center'>
                 <div className=" border-2 border-[#ABB2BF] h-24 w-0"></div>
                 <div className='flex flex-col gap-5'>
-                    <img src={github} alt="GitHub" />
-                    <img src={linkedin} alt="LinkedIn" />
-                    <img src={twitter} alt="Twitter" />
+                    <a href="https://github.com/chandan-d-karmaker" target='_blank' rel='noopener noreferrer'>
+                        <img src={github} alt="GitHub" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/chandankarmaker" target='_blank' rel='noopener noreferrer'>
+                        <img src={linkedin} alt="LinkedIn" />
+                    </a>
+                    <a href="https://x.com/chandank202" target='_blank' rel='noopener noreferrer'>
+                        <img src={twitter} alt="Twitter" />
+                    </a>
                 </div>
             </div>
         </div>
