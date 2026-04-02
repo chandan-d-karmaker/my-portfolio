5e500ffff21533c64dc5b3622dc9c96370b0ea40
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-02 23:47:37 +0600

    made custom mobile navbar

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import github from '../assets/Github.svg'
 import linkedin from '../assets/Linkedin.svg'
@@ -8,6 +8,9 @@ import ck from '../assets/ck.svg'
 
 
 const TopNavBar = () => {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
 
@@ -56,25 +59,50 @@ const TopNavBar = () => {
           </ul>
         </div>
 
-        <div className="dropdown navbar-end lg:hidden relative">
-          <div tabIndex={0} role="button" className="btn btn-ghost ">
+        <div className="navbar-end lg:hidden relative">
+          <div tabIndex={0} role="button" className="btn btn-ghost" onClick={() => setOpen(!open)}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
           </div>
           <ul
-            tabIndex="-1"
-            className="dropdown-content pt-20 pl-5 bg-[#282C33] z-40 min-h-screen absolute top-10 -left-40 min-w-lvw space-y-8">
-            <li><a href="" className='text-3xl'><span className='text-[#C778DD]'>#</span>home</a></li>
-            <li><a href="" className='text-3xl'><span className='text-[#C778DD]'>#</span>works</a></li>
-            <li><a href="" className='text-3xl'><span className='text-[#C778DD]'>#</span>about-me</a></li>
-            <li><a href="" className='text-3xl'><span className='text-[#C778DD]'>#</span>contacts</a></li>
+            className={`md:hidden duration-700 ${open ? 'top-14' : '-top-200'} absolute text-[#ABB2BF] min-w-screen p-6 pt-15 bg-[#282C33] z-40 min-h-screen space-y-8`}>
+            <li >
+              <Link to='hero' smooth={true}
+              duration={800}
+              offset={-70} // Useful for fixed headers
+              activeClass="active"
+              spy={true} className='cursor-pointer text-3xl' onClick={()=> setOpen(!open)}><span className='text-[#C778DD]'>#</span>home</Link>
+            </li>
+
+            <li>
+              <Link to='projects' smooth={true}
+                duration={800}
+                offset={-70} // Useful for fixed headers
+                activeClass="active"
+                spy={true} className='cursor-pointer text-3xl' onClick={()=> setOpen(!open)}><span className='text-[#C778DD]'>#</span>works</Link>
+            </li>
+
+            <li>
+              <Link to='about-me' smooth={true}
+                duration={800}
+                offset={-70} // Useful for fixed headers
+                activeClass="active"
+                spy={true} className='cursor-pointer text-3xl' onClick={()=> setOpen(!open)}><span className='text-[#C778DD]'>#</span>about-me</Link>
+            </li>
+            <li>
+              <Link to='contacts' smooth={true}
+                duration={800}
+                offset={-70} // Useful for fixed headers
+                activeClass="active"
+                spy={true} className='cursor-pointer text-3xl' onClick={()=> setOpen(!open)}><span className='text-[#C778DD]'>#</span>contacts</Link>
+            </li>
             <ul className='flex gap-5 mt-38 z-30'>
-              <li><a href="https://github.com/chandan-d-karmaker" target='_blank' rel='noopener noreferrer'>
+              <li><a href="https://github.com/chandan-d-karmaker" target='_blank' rel='noopener noreferrer' onClick={()=> setOpen(!open)}>
                 <img src={github} alt="GitHub" />
               </a></li>
-              <li><a href="https://www.linkedin.com/in/chandankarmaker" target='_blank' rel='noopener noreferrer'>
+              <li><a href="https://www.linkedin.com/in/chandankarmaker" target='_blank' rel='noopener noreferrer' onClick={()=> setOpen(!open)}>
                 <img src={linkedin} alt="LinkedIn" />
               </a></li>
-              <li><a href="https://x.com/chandank202" target='_blank' rel='noopener noreferrer'>
+              <li><a href="https://x.com/chandank202" target='_blank' rel='noopener noreferrer' onClick={()=> setOpen(!open)}>
                 <img src={twitter} alt="Twitter" />
               </a></li>
             </ul>
